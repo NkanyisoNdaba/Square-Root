efb0975524424aa2776715cbca19f15c5f7e339c
nkanyiso <ndaba.nkanyi@live.co.uk>
2018-07-30 08:58:32 +0200

    done

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbersList = new List<Square>
+            {
+                new Square{Number = 1},
+                new Square{Number = 2},
+                new Square{Number = 3},
+                new Square{Number = 4},
+                new Square{Number = 15},
+                new Square{Number = 6},
+                new Square{Number = 7},
+                new Square{Number = 18},
+                new Square{Number = 9},
+            };
+
+
+            var findNumbers = from Numbers in numbersList where Numbers.Number <= 9 && Numbers.Number > 0 select Numbers.Number;
+
+
+           
+            var square = from Square in numbersList
+                where Square.Number;
+        }
+    }
+}
